894f5e4f54110cca3a46e8794bacc5c920eb9005
fathimabushra198-boop <fathimabushra198@gmail.com>
2025-07-31 14:37:07 +0530

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,40 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello everyone")
+hello everyone
+num =7
+print(num)
+7
+type(num)
+<class 'int'>
+num=3.5
+type(num)
+<class 'float'>
+>>> num =a
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    num =a
+NameError: name 'a' is not defined
+>>> num=int (input("enter the value:"))
+enter the value:9
+>>> 
+>>> print (num**3)
+729
+>>> type(num )
+<class 'int'>
+>>> name="raheeba"
+>>> print("name;")
+name;
+>>> print("name:",raheeba)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    print("name:",raheeba)
+NameError: name 'raheeba' is not defined
+>>> print("name:",name)
+name: raheeba
+>>> name[4]
+'e'
+>>> name[-2]
+'b'
+>>> name.capitalize()
+'Raheeba'
