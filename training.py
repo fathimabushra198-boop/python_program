da01458a59328f7729d76aa3adfda5ad0e883bc5
fathimabushra198-boop <fathimabushra198@gmail.com>
2025-07-31 14:41:04 +0530

    Create training.py

---
+++ training.py
@@ -0,0 +1,4 @@
+Training=[['intro of com','tech skills' ,'and digital foundation'],['intro of python ','flowchart','and py data structure'],['loops and functions','intro of cloud','and internet']]
+print("my 3-days of training schedule:")
+for day,topic in enumerate(training,start=1):
+  print(day,topic)
