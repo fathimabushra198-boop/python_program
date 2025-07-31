8303d78158bed4a4148ad32e72e26f9f8a762108
fathimabushra198-boop <fathimabushra198@gmail.com>
2025-07-31 14:33:20 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,13 @@
+
+    
+
+
+
+         ALGORITHM:
+                 1.Start.
+                2.Declaration. 
+                3.Check weather the condition is ture or false . 
+                4.If the statement is ture the condition execute. 
+                5.Otherwise, go back to the declaration part body of the loop.
+                6. End.
+         
